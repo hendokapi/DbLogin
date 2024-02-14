a70663fb877acaae21d635985e9c34e8ad26d503
hendokapi <152817393+hendokapi@users.noreply.github.com>
2024-02-14 17:48:14 +0400

    Add files via upload

---
+++ SimpleLogin/Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleLogin
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserId"] != null)
+            {
+                Response.Redirect("Home.aspx");
+            }
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string username = TxtUsername.Text;
+            string password = TxtPassword.Text;
+
+            string connString = ConfigurationManager.ConnectionStrings["DbLoginConnectionString"].ToString();
+            SqlConnection conn = new SqlConnection(connString);
+            try
+            {
+                conn.Open();
+                SqlCommand registerUser = new SqlCommand($"select Id from Users where Username='{username}' and Password='{password}'", conn);
+                string UserId = registerUser.ExecuteScalar().ToString();
+                Session["UserId"] = UserId;
+            }
+            catch
+            {
+                Response.Write("qualcosa non va");
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
+            Response.Redirect("Home.aspx");
+        }
+    }
+}
---
+++ SimpleLogin/Templates/LoggedTemplate.master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleLogin
+{
+    public partial class LoggedTemplate : System.Web.UI.MasterPage
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (Session["UserId"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            LblUsername.Text = Session["UserId"].ToString();
+        }
+
+        protected void BtnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Remove("UserId");
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
